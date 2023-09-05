d21e8e652765fbe2f385524b0dc75bb48ec12ffb
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-09-05 17:43:10 +0530

    Good Question for DP as well as Trie

---
+++ Word Break 2.py
@@ -0,0 +1,22 @@
+Problem Link -> https://leetcode.com/problems/word-break-ii/
+
+def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def helper(s,wordDict,memo):
+            if s in memo:
+                return memo[s]
+            if not s:
+                return []
+            res = []
+            for word in wordDict:
+                if not s.startswith(word):
+                    continue
+                if len(word) == len(s):
+                    res.append(word)
+                else:
+                    resultOfTheRest = helper(s[len(word):],wordDict,memo)
+                    for item in resultOfTheRest:
+                        item = word + ' '+item
+                        res.append(item)
+            memo[s] = res
+            return res
+        return helper(s,wordDict,{})
