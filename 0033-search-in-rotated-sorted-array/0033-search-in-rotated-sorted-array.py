e4109d85e2ea2894f3a43732e9a36bbf1488ea4b
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-09-11 10:06:26 +0530

    Time: 45 ms (78.1%), Space: 16.8 MB (5.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -17,5 +17,38 @@ class Solution:
                 low = mid+1
         return -1
 
+        # Using Extra space to sotre the list elements in sorted array
+        breakPoint = -1
+        mp,index = {},0
+        for i in nums:
+            mp[i] = index
+            index += 1
+        for i in range(1,len(nums)):
+            if nums[i-1] > nums[i]:
+                breakPoint = i
+                break
+        temp = []
+        index = breakPoint
+        while index < len(nums):
+            temp.appene(nums[index])
+            index += 1
+        index = breakPoint-1
+        while index >= 0:
+            temp.append(nums[index])
+            index -= 1
+        low,high = 0,len(nums)-1
+        while low <= high:
+            mid = (low+high)//2
+            if temp[mid] == target:
+                return mp[temp[mid]]
+            elif nums[mid] > target:
+                high = mid-1
+            else:
+                low = mid+1
+        return -1
+        
+        
+
+
         
         
