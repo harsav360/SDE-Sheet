aac020e27a0fadfe460d180b3f02e14f335ce822
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-09-27 10:44:36 +0530

    Time: 106 ms (26.49%), Space: 21 MB (62.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,30 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # dct = {}
+        # for i in nums:
+        #     dct[i] = 1 + dct.get(i,0)
+        # temp = []
+        # for key in dct.keys():
+        #     temp.append([dct[key],key])
+        # temp.sort(reverse = True)
+        # ans = []
+        # i = 0
+        # while k:
+        #     ans.append(temp[i][1])
+        #     k -= 1
+        #     i += 1
+        # return ans
         dct = {}
         for i in nums:
             dct[i] = 1 + dct.get(i,0)
-        temp = []
+        minHeap = []
         for key in dct.keys():
-            temp.append([dct[key],key])
-        temp.sort(reverse = True)
+            heapq.heappush(minHeap,(-1*dct[key],key))
         ans = []
-        i = 0
         while k:
-            ans.append(temp[i][1])
+            first,second = heapq.heappop(minHeap)
+            ans.append(second)
             k -= 1
-            i += 1
         return ans
+
         
