d3fb465e1cb4356641dd91ef34902ae6caa23760
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-04 22:05:42 +0530

    Time: 720 ms (30.34%), Space: 78.6 MB (22.74%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,37 +1,48 @@
+class Node:
+    def __init__(self,key,val):
+        self.key,self.val = key,val
+        self.prev = self.next = None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.stack = []
-        self.ptr = 0
-        self.capacity = capacity
-        self.dct = {}
+        self.cap = capacity
+        self.cache = {}
+        # Left = lru, right = mru
+        self.left,self.right = Node(0,0),Node(0,0)
+        self.left.next,self.right.prev = self.right,self.left
 
-        
+    def remove(self,node):
+        prev,nxt = node.prev,node.next
+        prev.next,nxt.prev = nxt,prev
+    def insert(self,node):
+        prev,nxt = self.right.prev,self.right
+        prev.next = nxt.prev = node
+        node.next,node.prev = nxt,prev
+
 
     def get(self, key: int) -> int:
-        ans = -1
-        if key in self.dct:
-            ans = self.dct[key]
-            self.stack.remove(key)
-            self.stack.insert(0,key)
-        return ans
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    
+        
+
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dct:
-            self.dct[key] = value
-            self.stack.remove(key)
-            self.stack.insert(0,key)
-        else:
-            self.dct[key] = value
-            if self.ptr == self.capacity:
-                rem = self.stack.pop()
-                self.dct.pop(rem)
-                self.ptr -= 1
-            self.stack.insert(0,key)
-            self.ptr += 1
-            
-            
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
         
 
 
