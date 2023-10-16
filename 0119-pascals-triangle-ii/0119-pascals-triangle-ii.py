de19556d286b9f905e85a53d099b3adc7bd97ab9
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-16 11:47:01 +0530

    Time: 37 ms (68.36%), Space: 16.1 MB (96.64%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,14 +1,26 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        def ncr(n,r):
-            res = 1
-            for i in range(r):
-                res = res * (n-i)
-                res //= (i+1)
-            return res
+        # def ncr(n,r):
+        #     res = 1
+        #     for i in range(r):
+        #         res = res * (n-i)
+        #         res //= (i+1)
+        #     return res
 
-        ans = []
-        for c in range(1,rowIndex+2):
-            ans.append(ncr(rowIndex,c-1))
-        return ans
+        # ans = []
+        # for c in range(1,rowIndex+2):
+        #     ans.append(ncr(rowIndex,c-1))
+        # return ans
+
+        # Optimized Approach
+
+        result = []
+        ans = 1
+        result.append(ans)
+        rowIndex += 1
+        for i in range(1,rowIndex):
+            ans = ans*(rowIndex-i)
+            ans = ans//i
+            result.append(ans)
+        return result
         
