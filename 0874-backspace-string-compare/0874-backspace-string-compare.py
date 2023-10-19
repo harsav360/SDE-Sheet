68662d7de0e9d466b534e0c09e09b2a65eb1fe22
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-19 23:02:16 +0530

    Time: 40 ms (48.44%), Space: 16.2 MB (54.52%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -19,11 +19,37 @@ class Solution:
         #     return False
         # return True
 
-        def back(res,c):
-            if c != '#':res.append(c)
-            elif res:res.pop()
-            return res
-        return reduce(back,s,[]) == reduce(back,t,[])
+        # def back(res,c):
+        #     if c != '#':res.append(c)
+        #     elif res:res.pop()
+        #     return res
+        # return reduce(back,s,[]) == reduce(back,t,[])
+
+        # Optimized Approach
+
+        def nextValidChar(str,index):
+            backspace = 0
+            while index >= 0:
+                if backspace == 0 and str[index] != '#':
+                    break
+                elif str[index] == '#':
+                    backspace += 1
+                else:
+                    backspace -= 1
+                index -= 1
+            return index
+
+        index_s,index_t = len(s)-1,len(t)-1
+        while index_s >= 0 or index_t >= 0:
+            index_s,index_t = nextValidChar(s,index_s),nextValidChar(t,index_t)
+            char_s = s[index_s] if index_s >= 0 else ""
+            char_t = t[index_t] if index_t >= 0 else ""
+
+            if char_s != char_t:
+                return False
+            index_s -= 1
+            index_t -= 1
+        return True
             
 
         
