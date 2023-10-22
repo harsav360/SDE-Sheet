045c7c93c4b4255721834b9231496d7197aeee69
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-22 14:53:09 +0530

    Time: 70 ms (17.77%), Space: 16.2 MB (97.88%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,16 +1,45 @@
 class Solution:
-    def repeatedStringMatch(self, A: str, B: str) -> int:
-        # Language ki akad ka solution
-        temp = ""
-        count = 0
-        while len(temp) < len(B):
-            temp += A
+    BASE = 1000000
+
+    def repeatedStringMatch(self, A, B):
+        if A == B:
+            return 1
+        count = 1
+        source = A
+        while len(source) < len(B):
             count += 1
-            if B in temp:
-                return count
-        temp += A
-        if B in temp:
+            source += A
+        if source == B:
+            return count
+        if self.Rabin_Karp(source, B) != -1:
+            return count
+        if self.Rabin_Karp(source + A, B) != -1:
             return count + 1
         return -1
+
+    def Rabin_Karp(self, source, target):
+        if source == "" or target == "":
+            return -1
+        m = len(target)
+        power = 1
+        for i in range(m):
+            power = (power * 31) % self.BASE
+        targetCode = 0
+        for i in range(m):
+            targetCode = (targetCode * 31 + ord(target[i])) % self.BASE
+        hashCode = 0
+        for i in range(len(source)):
+            hashCode = (hashCode * 31 + ord(source[i])) % self.BASE
+            if i < m - 1:
+                continue
+            if i >= m:
+                hashCode = (hashCode - ord(source[i - m]) * power) % self.BASE
+            if hashCode < 0:
+                hashCode += self.BASE
+            if hashCode == targetCode:
+                if source[i - m + 1:i - m + 1 + m] == target:
+                    return i - m + 1
+        return -1
+
 
         
