591b248f83012ab4522a36f77133f917bb9d8653
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-22 22:35:55 +0530

    Time: 1667 ms (32.34%), Space: 36.2 MB (25.96%) - LeetHub

--- 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.py
@@ -1,15 +1,28 @@
 class Solution:
-    def constrainedSubsetSum(self, A, k):
-        deque = collections.deque()
-        for i in range(len(A)):
-            A[i] += deque[0] if deque else 0
-            while len(deque) and A[i] > deque[-1]:
-                deque.pop()
-            if A[i] > 0:
-                deque.append(A[i])
-            if i >= k and deque and deque[0] == A[i - k]:
-                deque.popleft()
-        return max(A)
+    def constrainedSubsetSum(self, nums, k):
+        # Harsav360 = Please understand the working of Algorithm
+        # deque = collections.deque()
+        # for i in range(len(A)):
+        #     A[i] += deque[0] if deque else 0
+        #     while len(deque) and A[i] > deque[-1]:
+        #         deque.pop()
+        #     if A[i] > 0:
+        #         deque.append(A[i])
+        #     if i >= k and deque and deque[0] == A[i - k]:
+        #         deque.popleft()
+        # return max(A)
+
+        res = nums[0]
+        max_heap = [(-nums[0],0)]
+        for i in range(1,len(nums)):
+            while i-max_heap[0][1] > k:
+                heapq.heappop(max_heap)
+            cur_max = max(nums[i],nums[i]-max_heap[0][0])
+            res = max(res,cur_max)
+            heapq.heappush(max_heap,(-cur_max,i))
+        return res
+
+
 
 
         
