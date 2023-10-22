0777e39180d6440599c9079520ec753e6b161437
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-22 15:52:33 +0530

    Time: 41 ms (36.97%), Space: 16.2 MB (46.39%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,51 @@
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+        # left,right,nedLen = 0,0,len(needle)
+        # ptr = 0
+        # while right < len(haystack):
+        #     if haystack[right] == needle[ptr]:
+        #         ptr += 1
+        #         right += 1
+        #     else:
+        #         left += 1
+        #         right = left
+        #         ptr = 0
+        #     if ptr == nedLen:
+        #         return left
+        # return -1
+
+        # KMP Algorithm
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        left,right,nedLen = 0,0,len(needle)
-        ptr = 0
-        while right < len(haystack):
-            if haystack[right] == needle[ptr]:
-                ptr += 1
-                right += 1
+        if needle == "":
+            return 0
+        lps = [0] * len(needle)
+
+        prevLPS, i = 0, 1
+        while i < len(needle):
+            if needle[i] == needle[prevLPS]:
+                lps[i] = prevLPS + 1
+                prevLPS += 1
+                i += 1
+            elif prevLPS == 0:
+                lps[i] = 0
+                i += 1
             else:
-                left += 1
-                right = left
-                ptr = 0
-            if ptr == nedLen:
-                return left
+                prevLPS = lps[prevLPS - 1]
+
+        i = 0  # ptr for haystack
+        j = 0  # ptr for needle
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i, j = i + 1, j + 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+            if j == len(needle):
+                return i - len(needle)
         return -1
+
 
         
