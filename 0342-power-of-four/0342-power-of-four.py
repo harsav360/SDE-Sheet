424ff0e4b1976d186c103985531a6485c5a5053a
Annu Sharma <51308620+harsav360@users.noreply.github.com>
2023-10-23 12:50:21 +0530

    Time: 40 ms (52.29%), Space: 16.1 MB (70.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,12 +1,21 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         # Brute Force Approach
-        for i in range(16):
-            power = 4**i
-            if power == n:
-                return True
-            if power > n:
-                return False
-        return False
+        # for i in range(16):
+        #     power = 4**i
+        #     if power == n:
+        #         return True
+        #     if power > n:
+        #         return False
+        # return False
+
+        # Mathematical Approach
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+        
+        logBase4 = math.log(n)/math.log(4)
+        return logBase4 == int(logBase4)
 
         
